1e498bab6ff536835f2a02b482794245294eb78b
PashamAdwani <pasham_adwani11@yahoo.co.in>
2018-11-12 16:38:12 -0500

    modifying json file reading function

--- testpy.py
+++ testpy.py
@@ -1,9 +1,7 @@
 import pandas as pd
+import numpy as np
 import json
 
-with open(r'D:\Data Analysis\Project2\Project2\Data-Analysis-Project2\train.json') as data_file:
-    data_loaded = json.load(data_file)
-
-print(data_loaded)
+train_file=pd.read_json(r'D:\Data Analysis\Project2\Project2\Data-Analysis-Project2\train.json', orient='records')
     
 
